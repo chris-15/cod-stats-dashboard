602c047236899954026912825ccd7b13a491814e
Chris <christopher.sarm15@gmail.com>
2023-11-27 21:07:25 -0500

    adding prisma and prisma client

---
+++ lib/prismadb.ts
@@ -0,0 +1,16 @@
+//code from the prisma docs  on best practices for instantiating PrismaClient with Next.js
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
