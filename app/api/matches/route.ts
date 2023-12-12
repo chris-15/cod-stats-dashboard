5d0cf3a8b0de77279bdb0ec656c20f8e4bb92471
Chris <christopher.sarm15@gmail.com>
2023-12-12 10:39:10 -0500

    protecting route by checking if user is signed in

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -3,23 +3,22 @@ import prisma from "@/lib/prismadb";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-
-
 //creating/adding match statistics
 export async function POST(req: Request) {
-
   //getting the authenticated user session
   const session = await getServerSession(authOptions);
 
-  //if no session return the next response error  
-  if(!session) {
-    return NextResponse.json({error: "Not Authenticated"}, {status:401})
+  //if no session return the next response error
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
   }
 
-  const { gameMode, matchMap, kills, deaths, damage, win, time } = await req.json();
+  const { gameMode, matchMap, kills, deaths, damage, win, time } =
+    await req.json();
 
-  // hardcoded useremail for now for testing purposes
-  const userEmail = "chris.sarm15@gmail.com";
+  
+  const userEmail = session?.user?.email as string;
+
   //if game mode not specified, dont create
   if (!gameMode) {
     return NextResponse.json({ error: "Game mode required" }, { status: 500 });
@@ -41,25 +40,36 @@ export async function POST(req: Request) {
     console.log("Match Stats added");
     return NextResponse.json(newMatch);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return NextResponse.json({ message: "Error adding stats" });
   }
 }
 
-// gets all matches in db
-export async function GET() {
+// gets all matches in db for signed in user
+export async function GET(req: Request) {
+  const session = await getServerSession(authOptions);
+  //if no session return the next response error
+  if (!session) {
+    return NextResponse.json({ error: "Not Authenticated" }, { status: 401 });
+  }
+
+  const userEmail = session?.user?.email as string;
+
   try {
     const matches = await prisma.match.findMany({
+      where: {
+        userEmail: userEmail,
+      },
       include: {
         user: { select: { name: true } },
       },
       orderBy: {
         createdAt: "desc",
-      }
+      },
     });
-    return NextResponse.json(matches)
+    return NextResponse.json(matches);
   } catch (error) {
-    console.log(error)
-    return  NextResponse.json({ message: error}, {status: 500 });
+    console.log(error);
+    return NextResponse.json({ message: error }, { status: 500 });
   }
 }
