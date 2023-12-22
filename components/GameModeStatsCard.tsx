31cb1ecc5959a7b34337f6c06737da5e0044d100
Chris <christopher.sarm15@gmail.com>
2023-12-22 00:01:05 -0500

    adding daily stats tab to gamemodecard component

--- components/GameModeStatsCard.tsx
+++ components/GameModeStatsCard.tsx
@@ -10,7 +10,6 @@ import {
   calcAvgTimeByResult,
   calcHighestKill,
   calcSlayingEfficiency,
-  calcModeRecentKdRatio,
   calcAvgTime,
   calcModeMapCount,
 } from "@/lib/utils";
@@ -21,72 +20,94 @@ type GameModeStatsProp = {
 
 function GameModeStatsCard({ gameMode }: GameModeStatsProp) {
   const { matches } = useMatches();
-  //console.log(matches)
+
+  //sets state for tab for the card
+  const [tab, setTab] = useState("tab1");
+
   //filter to get the gamemodes matches
-  const filteredMatches = matches.filter(
-    (match) => match.gameMode === gameMode
-  );
+  const filteredMatches = matches.filter((match) => match.gameMode === gameMode);
   //if user doesnt have 10 matches then just use matches data
   const numMatches = filteredMatches.length < 10 ? filteredMatches.length : 10;
   // get last 10 recent matches
   const recentMatches = filteredMatches.slice(0, numMatches);
 
-  const [tab, setTab] = useState("tab1");
+  //filter to get daily matches
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-  const totalKdRatio = calcModeKdRatio(matches, gameMode);
-  const recentKdRatio = calcModeRecentKdRatio(matches, gameMode);
-  const winPercentage = calcWinPercentage( tab === "tab1" ? matches : recentMatches,
-  gameMode,)
+  const dailyMatches = filteredMatches.filter((match) => {
+    const todaysMatchDate = new Date(match.createdAt);
+    todaysMatchDate.setHours(0, 0, 0, 0); // doing this because you only want to compare dates not timestamps
+    return todaysMatchDate.getTime() === today.getTime();
+  });
+
+  // all the stat calculations below- depending on which tab is selected
+  const totalKdRatio = calcModeKdRatio(
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
+    gameMode
+  );
+  const winPercentage = calcWinPercentage(
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
+    gameMode
+  );
+
   const kdByWin = calcModeKdByResult(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode,
     true
   );
 
   const kdByLoss = calcModeKdByResult(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode,
     false
   );
 
   const totalKills = calcModeTotalKills(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode
   );
   const totalDeaths = calcModeTotalDeaths(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode
   );
   const avgTime = calcAvgTime(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode
   );
   const timeByWin = calcAvgTimeByResult(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode,
     true
   );
   const timeByLoss = calcAvgTimeByResult(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode,
     false
   );
+
+  console.log(timeByLoss);
   const killRecord = calcHighestKill(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode
   );
   const slayingEfficiency = calcSlayingEfficiency(
-    tab === "tab1" ? matches : recentMatches,
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
     gameMode
   );
-  const mapCount = calcModeMapCount(matches, gameMode);
+  const mapCount = calcModeMapCount(
+    tab === "tab1" ? matches : tab === "tab2" ? recentMatches : dailyMatches,
+    gameMode
+  );
 
   return (
     <section className="gamemode-stats-card bg-[#1B1B1B] border border-[#333333] rounded-lg p-4 relative">
       <div className="absolute top-0 left-0 flex space-x-2">
         <button
           className={`py-2 px-4 m-2 ${
-            tab === "tab1" ? "text-white border-b-2 border-white" : "text-gray-300"
+            tab === "tab1"
+              ? "text-white border-b-2 border-white"
+              : "text-gray-300"
           }`}
           onClick={() => setTab("tab1")}
         >
@@ -94,59 +115,30 @@ function GameModeStatsCard({ gameMode }: GameModeStatsProp) {
         </button>
         <button
           className={`py-2 px-4 m-2 ${
-            tab === "tab2" ? "text-white border-b-2 border-white" : "text-gray-300"
+            tab === "tab2"
+              ? "text-white border-b-2 border-white"
+              : "text-gray-300"
           }`}
           onClick={() => setTab("tab2")}
         >
           Last 10 Matches
+        </button>
+        <button
+          className={`py-2 px-4 m-2 ${
+            tab === "tab3"
+              ? "text-white border-b-2 border-white"
+              : "text-gray-300"
+          }`}
+          onClick={() => setTab("tab3")}
+        >
+          Daily
         </button>
       </div>
 
       <div className=" text-center grid grid-cols-2 md:grid-cols-3 gap-4 lg:flex lg:justify-evenly lg:flex-wrap mt-12">
         <div className="">
           <p className="">K/D</p>
-          {tab === "tab1" ? (
-            <p className="">{totalKdRatio}</p>
-          ) : (
-            <p>
-              {recentKdRatio}
-              <span className="">
-                {recentKdRatio > totalKdRatio ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 ml-1 mb-2 inline-block text-green-500 "
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
-                    />
-                  </svg>
-                ) : recentKdRatio < totalKdRatio ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6 inline-block text-red-500"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                ) : (
-                  ""
-                )}
-              </span>
-            </p>
-          )}
+          <p className="">{totalKdRatio}</p>
         </div>
 
         <div>
@@ -182,12 +174,12 @@ function GameModeStatsCard({ gameMode }: GameModeStatsProp) {
             </div>
             <div>
               <p>Avg Time in W</p>
-              <p className="">{timeByWin}</p>
+              <p className="">{timeByWin === "NaN:NaN" ? "--" : timeByWin}</p>
             </div>
 
             <div>
               <p>Avg Time in L</p>
-              <p className="">{timeByLoss}</p>
+              <p className="">{timeByLoss === "NaN:NaN" ? "--" : timeByLoss}</p>
             </div>
           </>
         )}
@@ -200,7 +192,7 @@ function GameModeStatsCard({ gameMode }: GameModeStatsProp) {
           <p>Slaying Efficiency</p>
           <p className="">{slayingEfficiency} %</p>
         </div>
-        {tab === "tab1" ? (
+        {tab === "tab1" || tab === "tab3" ? (
           <div>
             <p>Map Count</p>
             <p className="">{mapCount}</p>
