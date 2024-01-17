38b5e40d0a6647ddb82fb8beeb5e28b1d76243e3
Chris <christopher.sarm15@gmail.com>
2024-01-17 13:20:38 -0500

    adding different styles when no matches recored for matches tables

--- components/GameModeMatchesTable.tsx
+++ components/GameModeMatchesTable.tsx
@@ -25,68 +25,73 @@ function GameModeMatchesTable({ gameMode }: GameModeStatsProp) {
           All Matches
         </h2>
       </div>
-      <table className="">
-        <thead className="sticky top-0 z-10 bg-[#161B22]">
-          <tr>
-            <th>Date</th>
-            {/* <th className="text-center">Game Mode</th> */}
-            <th>Map</th>
-            <th>W/L Result</th>
-            <th>Kills</th>
-            <th>Deaths</th>
-            <th>Damage</th>
-            {gameMode === "Hardpoint" && <th>Time</th>}
-            {gameMode === "SearchAndDestroy" &&(
-              <>
-              <th>Plants</th>
-              <th>Defuses</th>  
-              </>
-              ) 
-            }
-
-            <th className="text-center">K/D Ratio</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gameModeMatches.map((match) => (
-            <tr className="text-center" key={match.id}>
-              <td>
-                {new Date(match.createdAt).toLocaleDateString("en-US", {
-                  year: "2-digit",
-                  month: "2-digit",
-                  day: "2-digit",
-                })}
-              </td>
-              <td>{match.matchMap}</td>
-              {match.win ? (
-                <td className="text-green-500">Win</td>
-              ) : (
-                <td className="text-red-500">Loss</td>
-              )}
-              <td>{match.kills}</td>
-              <td>{match.deaths}</td>
-              <td>{match.damage ? match.damage : "N/A"}</td>
-              {match.gameMode === "Hardpoint" && (
-                <td>{convertTime(match.time)}</td>
-              )}
-              {match.gameMode === "SearchAndDestroy" && (
+      {gameModeMatches.length > 0 ? (
+        <table className="">
+          <thead className="sticky top-0 z-10 bg-[#161B22]">
+            <tr>
+              <th>Date</th>
+              {/* <th className="text-center">Game Mode</th> */}
+              <th>Map</th>
+              <th>W/L Result</th>
+              <th>Kills</th>
+              <th>Deaths</th>
+              <th>Damage</th>
+              {gameMode === "Hardpoint" && <th>Time</th>}
+              {gameMode === "SearchAndDestroy" && (
                 <>
-                <td>{match.plants}</td>
-                <td>{match.defuses}</td>
+                  <th>Plants</th>
+                  <th>Defuses</th>
                 </>
               )}
-              <td>{(match.kills / match.deaths).toFixed(2)}</td>
-              <td className="flex justify-around items-center">
-                <Link href={`/edit-stats/${match.id}`}>
-                  <span className=" text-[#58a6FF] hover:underline">Edit</span>
-                </Link>
-                <MatchDeleteButton id={match.id}/>
-              </td>
+
+              <th className="text-center">K/D Ratio</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {gameModeMatches.map((match) => (
+              <tr className="text-center" key={match.id}>
+                <td>
+                  {new Date(match.createdAt).toLocaleDateString("en-US", {
+                    year: "2-digit",
+                    month: "2-digit",
+                    day: "2-digit",
+                  })}
+                </td>
+                <td>{match.matchMap}</td>
+                {match.win ? (
+                  <td className="text-green-500">Win</td>
+                ) : (
+                  <td className="text-red-500">Loss</td>
+                )}
+                <td>{match.kills}</td>
+                <td>{match.deaths}</td>
+                <td>{match.damage ? match.damage : "N/A"}</td>
+                {match.gameMode === "Hardpoint" && (
+                  <td>{convertTime(match.time)}</td>
+                )}
+                {match.gameMode === "SearchAndDestroy" && (
+                  <>
+                    <td>{match.plants}</td>
+                    <td>{match.defuses}</td>
+                  </>
+                )}
+                <td>{(match.kills / match.deaths).toFixed(2)}</td>
+                <td className="flex justify-around items-center">
+                  <Link href={`/edit-stats/${match.id}`}>
+                    <span className=" text-[#58a6FF] hover:underline">
+                      Edit
+                    </span>
+                  </Link>
+                  <MatchDeleteButton id={match.id} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center p-4">No Matches Recorded</p>
+      )}
     </section>
   );
 }
--- components/RecentMatchesTable.tsx
+++ components/RecentMatchesTable.tsx
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 import { TMatch } from "@/app/types";
 import { useMatches } from "./matchesContext";
-import { convertTime } from "../lib/utils"
-
+import { convertTime } from "../lib/utils";
 
 function RecentMatchesTable() {
   //calling custom useMatch hook to get the matches
   const { matches } = useMatches();
   //console.log(matches)
 
-  //new array that hold last 10 matches 
+  //new array that hold last 10 matches
   const lastTenMatches = matches ? matches.slice(0, 10) : [];
 
   return (
@@ -19,47 +18,52 @@ function RecentMatchesTable() {
           Recent Matches - Quick Stats
         </h2>
       </div>
-      {
-      matches.length > 0 ? <table className="">
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Game Mode</th>
-          <th>Map</th>
-          <th>W/L Result</th>
-          <th>Kills</th>
-          <th>Deaths</th>
-          {/* <th>Damage</th> */}
-         {/*  <th>Time</th> */}
-          <th>K/D Ratio</th>
-        </tr>
-      </thead>
-      <tbody>
-        {lastTenMatches.map((match) => (
-          <tr className="text-center" key={match.id}>
-            <td>
-              {new Date(match.createdAt).toLocaleDateString("en-US", {
-                year: "2-digit",
-                month: "2-digit",
-                day: "2-digit",
-              })}
-            </td>
-            <td>{match.gameMode === 'SearchAndDestroy' ? 'Search & Destroy' : match.gameMode }</td>
-            <td>{match.matchMap}</td>
-            {match.win ? (
-              <td className="text-green-500 ">Win</td>
-            ) : (
-              <td className="text-red-500 ">Loss</td>
-            )}
-            <td>{match.kills}</td>
-            <td>{match.deaths}</td>
-            <td>{(match.kills / match.deaths).toFixed(2)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table> :<p className="text-center p-4">No Matches Recorded</p>
-      }
-      
+      {matches.length > 0 ? (
+        <table className="">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Game Mode</th>
+              <th>Map</th>
+              <th>W/L Result</th>
+              <th>Kills</th>
+              <th>Deaths</th>
+              {/* <th>Damage</th> */}
+              {/*  <th>Time</th> */}
+              <th>K/D Ratio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lastTenMatches.map((match) => (
+              <tr className="text-center" key={match.id}>
+                <td>
+                  {new Date(match.createdAt).toLocaleDateString("en-US", {
+                    year: "2-digit",
+                    month: "2-digit",
+                    day: "2-digit",
+                  })}
+                </td>
+                <td>
+                  {match.gameMode === "SearchAndDestroy"
+                    ? "Search & Destroy"
+                    : match.gameMode}
+                </td>
+                <td>{match.matchMap}</td>
+                {match.win ? (
+                  <td className="text-green-500 ">Win</td>
+                ) : (
+                  <td className="text-red-500 ">Loss</td>
+                )}
+                <td>{match.kills}</td>
+                <td>{match.deaths}</td>
+                <td>{(match.kills / match.deaths).toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center p-4">No Matches Recorded</p>
+      )}
     </section>
   );
 }
