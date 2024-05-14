fb83a11ea00353b93e4d4d651f45a6b4a4f9f496
Chris <christopher.sarm15@gmail.com>
2024-05-14 10:40:56 -0400

    updating top cards calc functions

--- components/TopCards.tsx
+++ components/TopCards.tsx
@@ -1,26 +1,54 @@
-import { useMatches } from "./matchesContext";
 import { getMatches } from "@/server/queries";
-import {
-  calcModeKdRatio,
-  calcWinPercentage,
-  calcOverallKdRatio,
-  calcOverallWinPercentage,
-} from "@/lib/utils";
+import { calcOverallKdRatio, calcOverallWinPercentage } from "@/lib/utils";
 import TopCardGameMode from "./TopCardGameMode";
+import { TMatchQuery } from "@/app/types";
 
 async function TopCards() {
   const matches = await getMatches();
 
+  const calcKd = (matches: TMatchQuery[], gameMode: string) => {
+    let killSum = 0;
+    let deathSum = 0;
+
+    matches.forEach((obj) => {
+      if (obj.gameMode === gameMode) {
+        killSum += obj.kills;
+        deathSum += obj.deaths;
+      }
+    });
+
+    // if no deaths dont penalize kd ratio just divide by 1
+    const kdRatio = deathSum !== 0 ? +(killSum / deathSum) : +(killSum / 1);
+
+    return kdRatio.toFixed(2);
+  };
+
+  const calcWinByMode = (matches: TMatchQuery[], gameMode: string) => {
+    let winSum = 0;
+    let totalGames = 0;
+    matches.forEach((obj) => {
+      if (obj.gameMode === gameMode) {
+        // + operator turns obj.win = true => obj.win = 1
+        winSum += +obj.win;
+        totalGames++;
+      }
+    });
+
+    const winPercentage = +((winSum / totalGames) * 100).toFixed(1);
+
+    return totalGames > 0 ? winPercentage : 0;
+  };
+
   const overallKdRatio = calcOverallKdRatio(matches);
-  const hpKdRatio = calcModeKdRatio(matches, "Hardpoint");
-  const controlKdRatio = calcModeKdRatio(matches, "Control");
-  const searchKdRatio = calcModeKdRatio(matches, "SearchAndDestroy");
+  const hpKdRatio = calcKd(matches, "Hardpoint");
+  const controlKdRatio = calcKd(matches, "Control");
+  const searchKdRatio = calcKd(matches, "SearchAndDestroy");
 
   const overallWinPercentage = calcOverallWinPercentage(matches);
-  const hpWinPercentage = calcWinPercentage(matches, "Hardpoint");
-  const controlWinPercentage = calcWinPercentage(matches, "Control");
+  const hpWinPercentage = calcWinByMode(matches, "Hardpoint");
+  const controlWinPercentage = calcWinByMode(matches, "Control");
 
-  const searchWinPercentage = calcWinPercentage(matches, "SearchAndDestroy");
+  const searchWinPercentage = calcWinByMode(matches, "SearchAndDestroy");
 
   return (
     <div className="">
