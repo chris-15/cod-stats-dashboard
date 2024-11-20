b800f6764ed8454d4f5ea16e97cfa69acfc7c90a
Chris <christopher.sarm15@gmail.com>
2024-11-20 16:15:42 -0500

    fixing edit stats layout prop error

--- app/edit-stats/[id]/layout.tsx
+++ app/edit-stats/[id]/layout.tsx
@@ -15,17 +15,17 @@ export default function EditStatsLayout({
       <div className="flex flex-col min-h-screen">
         <div className="flex flex-1">
           <div className="hidden md:block">
-            <SideBar />
+            <SideBar game="bo6" />
           </div>
           <div className="flex flex-col flex-1">
             <div className="md:hidden">
-              <Navbar />
+              <Navbar game="bo6" />
             </div>
             <div className="flex-1 pb-16 sm:pb-0">{children}</div>
           </div>
         </div>
         <div className="sm:hidden fixed bottom-0 left-0 right-0 z-50 ">
-          <MobileNav />
+          <MobileNav game="bo6" />
         </div>
       </div>
       <Toaster />
