66a745033a3384db3e3bfbccf3da5ff61a59cc5a
Chris <christopher.sarm15@gmail.com>
2025-02-28 19:22:48 -0500

    removing commmented out code

--- components/GameModeMapStats.tsx
+++ components/GameModeMapStats.tsx
@@ -126,7 +126,7 @@ function GameModeMapStats({ gameMode, matches, game }: GameModeStatsProp) {
     return bo6MapSets[gameMode][mapsMatchesArr.indexOf(bestMap)];
   }
 
-  function calcWorstMap(mapsMatchesArr: TMatchQuery[][]){
+  function calcWorstMap(mapsMatchesArr: TMatchQuery[][]) {
     let worstMap = mapsMatchesArr[0];
     let worstScore = calcMapScore(worstMap, gameMode);
 
@@ -140,9 +140,7 @@ function GameModeMapStats({ gameMode, matches, game }: GameModeStatsProp) {
     return mapSets[gameMode][mapsMatchesArr.indexOf(worstMap)];
   }
 
-  
-
-  function calcWorstBo6Map(mapsMatchesArr: TMatchQuery[][]){
+  function calcWorstBo6Map(mapsMatchesArr: TMatchQuery[][]) {
     let worstMap = mapsMatchesArr[0];
     let worstScore = calcMapScore(worstMap, gameMode);
 
@@ -186,276 +184,136 @@ function GameModeMapStats({ gameMode, matches, game }: GameModeStatsProp) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {game === "bo6" ? (
-            bo6MapModeMatches.map((matches, index) => (
-              <TableRow
-                key={index}
-                className="hover:bg-zinc-800/50 border-zinc-800"
-              >
-                <TableCell className="sticky left-0 bg-secondary-bg">
-                  {bo6MapSets[gameMode][index] === bestBo6Map ? (
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Badge variant="best">
-                            {bo6MapSets[gameMode][index]}
-                          </Badge>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>This is your best performing map!</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  ) :
-                  bo6MapSets[gameMode][index] === worstBo6Map ? 
-                  (<TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Badge variant="worst">
-                            {bo6MapSets[gameMode][index]}
-                          </Badge>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>This is your worst performing map!</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>) : 
-                    <>{bo6MapSets[gameMode][index]}</>
-                }
-            
-                </TableCell>
-                <TableCell>{calcWinPercentage(matches)}</TableCell>
-                <TableCell>{calcModeKdRatio(matches)}</TableCell>
-                {gameMode === "Hardpoint" && (
-                  <TableCell>
-                    {calcAvgTime(matches) === "NaN:NaN"
-                      ? "--"
-                      : calcAvgTime(matches)}
+          {game === "bo6"
+            ? bo6MapModeMatches.map((matches, index) => (
+                <TableRow
+                  key={index}
+                  className="hover:bg-zinc-800/50 border-zinc-800"
+                >
+                  <TableCell className="sticky left-0 bg-secondary-bg">
+                    {bo6MapSets[gameMode][index] === bestBo6Map ? (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Badge variant="best">
+                              {bo6MapSets[gameMode][index]}
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>This is your best performing map!</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : bo6MapSets[gameMode][index] === worstBo6Map ? (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Badge variant="worst">
+                              {bo6MapSets[gameMode][index]}
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>This is your worst performing map!</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : (
+                      <>{bo6MapSets[gameMode][index]}</>
+                    )}
                   </TableCell>
-                )}
-                {gameMode === "SearchAndDestroy" && (
-                  <>
+                  <TableCell>{calcWinPercentage(matches)}</TableCell>
+                  <TableCell>{calcModeKdRatio(matches)}</TableCell>
+                  {gameMode === "Hardpoint" && (
                     <TableCell>
-                      {!calcAvgPlants(matches) ? "--" : calcAvgPlants(matches)}
-                    </TableCell>
-                    <TableCell>
-                      {!calcAvgDefuses(matches)
+                      {calcAvgTime(matches) === "NaN:NaN"
                         ? "--"
-                        : calcAvgDefuses(matches)}
+                        : calcAvgTime(matches)}
                     </TableCell>
-                  </>
-                )}
-                <TableCell>{calcAvgKills(matches)}</TableCell>
-                <TableCell>{calcHighestKill(matches)}</TableCell>
-              </TableRow>
-            ))
-          ) : 
-          (
-            mapModeMatches.map((matches, index) => (
-              <TableRow
-                key={index}
-                className="hover:bg-zinc-800/50 border-zinc-800"
-              >
-                <TableCell className="sticky left-0 bg-secondary-bg">
-                  {mapSets[gameMode][index] === bestMap ? (
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Badge variant="best">
-                            {mapSets[gameMode][index]}
-                          </Badge>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>This is your best performing map!</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  ) :
-                  mapSets[gameMode][index] === worstMap ? 
-                  (<TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Badge variant="worst">
-                            {mapSets[gameMode][index]}
-                          </Badge>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>This is your worst performing map!</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>) : 
-                    <>{mapSets[gameMode][index]}</>
-                }
-            
-                </TableCell>
-                <TableCell>{calcWinPercentage(matches)}</TableCell>
-                <TableCell>{calcModeKdRatio(matches)}</TableCell>
-                {gameMode === "Hardpoint" && (
-                  <TableCell>
-                    {calcAvgTime(matches) === "NaN:NaN"
-                      ? "--"
-                      : calcAvgTime(matches)}
+                  )}
+                  {gameMode === "SearchAndDestroy" && (
+                    <>
+                      <TableCell>
+                        {!calcAvgPlants(matches)
+                          ? "--"
+                          : calcAvgPlants(matches)}
+                      </TableCell>
+                      <TableCell>
+                        {!calcAvgDefuses(matches)
+                          ? "--"
+                          : calcAvgDefuses(matches)}
+                      </TableCell>
+                    </>
+                  )}
+                  <TableCell>{calcAvgKills(matches)}</TableCell>
+                  <TableCell>{calcHighestKill(matches)}</TableCell>
+                </TableRow>
+              ))
+            : mapModeMatches.map((matches, index) => (
+                <TableRow
+                  key={index}
+                  className="hover:bg-zinc-800/50 border-zinc-800"
+                >
+                  <TableCell className="sticky left-0 bg-secondary-bg">
+                    {mapSets[gameMode][index] === bestMap ? (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Badge variant="best">
+                              {mapSets[gameMode][index]}
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>This is your best performing map!</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : mapSets[gameMode][index] === worstMap ? (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <Badge variant="worst">
+                              {mapSets[gameMode][index]}
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>This is your worst performing map!</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : (
+                      <>{mapSets[gameMode][index]}</>
+                    )}
                   </TableCell>
-                )}
-                {gameMode === "SearchAndDestroy" && (
-                  <>
+                  <TableCell>{calcWinPercentage(matches)}</TableCell>
+                  <TableCell>{calcModeKdRatio(matches)}</TableCell>
+                  {gameMode === "Hardpoint" && (
                     <TableCell>
-                      {!calcAvgPlants(matches) ? "--" : calcAvgPlants(matches)}
-                    </TableCell>
-                    <TableCell>
-                      {!calcAvgDefuses(matches)
+                      {calcAvgTime(matches) === "NaN:NaN"
                         ? "--"
-                        : calcAvgDefuses(matches)}
+                        : calcAvgTime(matches)}
                     </TableCell>
-                  </>
-                )}
-                <TableCell>{calcAvgKills(matches)}</TableCell>
-                <TableCell>{calcHighestKill(matches)}</TableCell>
-              </TableRow>
-            ))
-          )
-          }
+                  )}
+                  {gameMode === "SearchAndDestroy" && (
+                    <>
+                      <TableCell>
+                        {!calcAvgPlants(matches)
+                          ? "--"
+                          : calcAvgPlants(matches)}
+                      </TableCell>
+                      <TableCell>
+                        {!calcAvgDefuses(matches)
+                          ? "--"
+                          : calcAvgDefuses(matches)}
+                      </TableCell>
+                    </>
+                  )}
+                  <TableCell>{calcAvgKills(matches)}</TableCell>
+                  <TableCell>{calcHighestKill(matches)}</TableCell>
+                </TableRow>
+              ))}
         </TableBody>
       </Table>
     </section>
-    /*  <section className="w-full bg-secondary-bg border border-[#444444] rounded-lg overflow-auto">
-      <div className=" px-4 sm:px-6 py-4 sticky left-0">
-        <h2 className="text-lg sm:text-xl font-bold ">Map Stats</h2>
-      </div>
-      <div className="">
-        <Table className="w-full">
-          <TableHeader className="bg-zinc-800/50">
-            <TableRow className="hover:bg-zinc-800/50 border-zinc-800 ">
-       
-              <TableHead className="sticky left-0 ">Map</TableHead>
-
-              <TableHead>Win %</TableHead>
-              <TableHead>K/D Ratio</TableHead>
-
-            
-              {gameMode === "Hardpoint" && <th>Avg Time</th>}
-              {gameMode === "SearchAndDestroy" && (
-                <>
-                  <TableHead>Avg Plants</TableHead>
-                  <TableHead>Avg Defuses</TableHead>
-                </>
-              )}
-              {gameMode === "Control" && <th>Avg Damage</th>}
-
-              <TableHead>Avg Kills</TableHead>
-              <TableHead>Kill Record</TableHead>
-              <TableHead className="xs:hidden">Map Count</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {game === "bo6"
-              ? bo6MapModeMatches.map((matches, index) => (
-                  <TableRow className="text-center" key={index}>
-               
-                    <TableCell className="sticky left-0 bg-secondary-bg">
-                      {bo6MapSets[gameMode][index]}
-                    </TableCell>
-
-                    <TableCell>{calcWinPercentage(matches)}</TableCell>
-                    <TableCell>{calcModeKdRatio(matches)}</TableCell>
-
-                
-                    {gameMode === "Hardpoint" && (
-                      <TableCell>
-                        {calcAvgTime(matches) === "NaN:NaN"
-                          ? "--"
-                          : calcAvgTime(matches)}
-                      </TableCell>
-                    )}
-                    {gameMode === "SearchAndDestroy" && (
-                      <>
-                        <TableCell>
-                          {!calcAvgPlants(matches)
-                            ? "--"
-                            : calcAvgPlants(matches)}
-                        </TableCell>
-                        <TableCell>
-                          {!calcAvgDefuses(matches)
-                            ? "--"
-                            : calcAvgDefuses(matches)}
-                        </TableCell>
-                      </>
-                    )}
-                    {gameMode === "Control" && (
-                      <TableCell>{calcAvgDamage(matches)}</TableCell>
-                    )}
-                    <TableCell>{calcAvgKills(matches)}</TableCell>
-                    <TableCell>{calcHighestKill(matches)}</TableCell>
-                    <TableCell className="xs:hidden">
-                      {matches.length}
-                    </TableCell>
-                  </TableRow>
-                ))
-              : mapModeMatches.map((matches, index) => (
-                  <TableRow className="text-center" key={index}>
-              
-                    <TableCell className="sticky left-0 bg-secondary-bg">
-                      {mapSets[gameMode][index]}
-                    </TableCell>
-
-                    <TableCell>{calcWinPercentage(matches)}</TableCell>
-                    <TableCell>{calcModeKdRatio(matches)}</TableCell>
-
-                    <TableCell className="xs:hidden">
-                      {calcModeKdRatio(matches, true)}
-                    </TableCell>
-                    <TableCell className="xs:hidden">
-                      {calcModeKdRatio(matches, false)}
-                    </TableCell>
-                    {gameMode === "Hardpoint" && (
-                      <TableCell>
-                        {calcAvgTime(matches) === "NaN:NaN"
-                          ? "--"
-                          : calcAvgTime(matches)}
-                      </TableCell>
-                    )}
-                    {gameMode === "SearchAndDestroy" && (
-                      <>
-                        <TableCell>
-                          {!calcAvgPlants(matches)
-                            ? "--"
-                            : calcAvgPlants(matches)}
-                        </TableCell>
-                        <TableCell>
-                          {!calcAvgDefuses(matches)
-                            ? "--"
-                            : calcAvgDefuses(matches)}
-                        </TableCell>
-                      </>
-                    )}
-                    {gameMode === "Control" && (
-                      <TableCell>{calcAvgDamage(matches)}</TableCell>
-                    )}
-                    <TableCell>{calcAvgKills(matches)}</TableCell>
-                    <TableCell>{calcHighestKill(matches)}</TableCell>
-                    <TableCell className="xs:hidden">
-                      {matches.length}
-                    </TableCell>
-                  </TableRow>
-                ))}
-          </TableBody>
-        </Table>
-      </div>
-    </section> */
   );
 }
 export default GameModeMapStats;
-
-{
-  /* {mapSets[gameMode][index] === bestMap ? (
-              <td
-                data-tooltip-id="mapstats-tooltip-id"
-                data-tooltip-content="This is your best map for this game mode!"
-                className=""
-              >
-                ⭐️
-              </td>
-            ) : (
-              <td className=""></td>
-            )} */
-}
