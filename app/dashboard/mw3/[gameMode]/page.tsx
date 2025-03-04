c5e6553b0ead8746c5396da5466ee054b47d1ee6
Chris <christopher.sarm15@gmail.com>
2025-03-04 16:03:58 -0500

    bg color changes and border color changes

--- app/dashboard/mw3/[gameMode]/page.tsx
+++ app/dashboard/mw3/[gameMode]/page.tsx
@@ -91,7 +91,7 @@ async function GameModeStatsPage({ params }: { params: { gameMode: string } }) {
             />
 
             <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-              <div className="grid grid-cols-2 bg-secondary-bg border border-[#444444] rounded-lg divide-x divide-[#444444] ">
+              <div className="grid grid-cols-2 bg-sidebar border rounded-lg divide-x divide-[#444444] ">
                 <div>
                   <h3 className="text-center pt-4 text-lg sm:text-xl font-bold">
                     Top 10 Kills
@@ -107,11 +107,11 @@ async function GameModeStatsPage({ params }: { params: { gameMode: string } }) {
               </div>
 
               <div className="hidden xs:flex flex-col justify-between space-y-4 xl:space-y-2">
-                <div className="border border-[#444444] rounded-lg bg-secondary-bg hidden xs:block">
+                <div className="border rounded-lg bg-sidebar hidden xs:block">
                   <h2 className="text-center pt-4">Match Count by Map</h2>
                   <MapBarChartComponent data={mapCountData} fill="#b0ff34" />
                 </div>
-                <div className="bg-secondary-bg border border-[#444444] rounded-lg">
+                <div className="bg-sidebar border rounded-lg">
                   <h2 className="text-center pt-4">KD by Result by Map</h2>
                   <KdBarChartComponent matches={matches} />
                 </div>
