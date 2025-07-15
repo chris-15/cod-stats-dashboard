a0302425bc1910d202a7737c2227f897548b7c28
Chris <christopher.sarm15@gmail.com>
2025-07-15 18:27:56 -0400

    deleting unneeded placeholder folder with old code

--- placeholder/bo6/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <span className="loader-bo6"></span>
-        </div>
-    )
-  }
